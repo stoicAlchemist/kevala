81a9886cbc2f4ba79a9aba90e31721a84bb097f2
Brian Ivan Martinez Estrada <1622554+stoicAlchemist@users.noreply.github.com>
2024-09-20 22:35:07 -0600

    fix(shifts): Should not assign if different positions

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,10 +1,15 @@
 class Shift < ApplicationRecord
   belongs_to :location
   belongs_to :assigned_employee, class_name: "Employee", optional: true
+  validate :assigned_employee_position, on: :create
 
   enum :position, {
     rn: "rn",
     lpn: "lpn",
     cna: "cna"
   }
+
+  def assigned_employee_position
+    errors.add(:assigned_employee_id, "should have the same position") unless position == assigned_employee.position
+  end
 end
--- test/models/shift_test.rb
+++ test/models/shift_test.rb
@@ -4,4 +4,18 @@ class ShiftTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "assigned_employee should have the same position" do
+    location = Location.create(name: "Location 1")
+    employee = Employee.create(name: "Employee rn", position: "rn", location: location)
+    shift = Shift.new(
+      starts_at: Time.now + 2.days,
+      ends_at: Time.now + 3.days,
+      location_id: location.id,
+      assigned_employee_id: employee.id,
+      position: "lpn"
+    )
+
+    assert_not shift.save, "assigned to employee with different position"
+  end
 end
